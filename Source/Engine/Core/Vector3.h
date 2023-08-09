c60b7a83a399479b1cef77e3deaafbe819d596f8
NeuHezxp <mpatino@student.neumont.edu>
2023-08-09 13:31:10 -0600

    added z dimension to vector3

--- Source/Engine/Core/Vector3.h
+++ Source/Engine/Core/Vector3.h
@@ -1,96 +1,73 @@
 #pragma once
+#pragma once
 #include <cmath>
-#include <istream>
 #include <sstream>
 
 namespace kiko
 {
 	class Vector3
 	{
+	public: //hey!! you can access x and y here
+		float x, y, z;
+
 	public:
-		float x, y;
+		Vector3() : x{ 0 }, y{ 0 }, z{ 0 } {}
+		Vector3(float v) : x{ v }, y{ v }, z{ v } {} //sets the x AND y to v
+		Vector3(float x, float y, float z) : x{ x }, y{ y }, z{ z } {}
+		Vector3(int x, int y, int z) : x{ (float)x }, y{ (float)y }, z{ (float)z } {}
 
-		Vector2() : x{ 0 }, y{ 0 } {}
-		Vector2(float v) : x{ v }, y{ v } {}
-		Vector2(float x, float y) : x{ x }, y{ y } {}
-		Vector2(int x, int y) : x{ (float)x }, y{ (float)y } {}
+		Vector3 operator + (const Vector3& v) const { return Vector3(x + v.x, y + v.y, z + v.z); };
+		Vector3 operator - (const Vector3& v) const { return Vector3(x - v.x, y - v.y, z - v.z); };
+		Vector3 operator * (const Vector3& v) const { return Vector3(x * v.x, y * v.y, z * v.z); };
+		Vector3 operator / (const Vector3& v) const { return Vector3(x / v.x, y / v.y, z / v.z); };
 
-		Vector2 operator + (const Vector2& v) const { return Vector2(x + v.x, y + v.y); }
-		Vector2 operator - (const Vector2& v) const { return Vector2(x - v.x, y - v.y); }
-		Vector2 operator / (const Vector2& v) const { return Vector2(x / v.x, y / v.y); }
-		Vector2 operator * (const Vector2& v) const { return Vector2(x * v.x, y * v.y); }
+		Vector3 operator + (float s) const { return Vector3(x + s, y + s, z + s); };
+		Vector3 operator - (float s) const { return Vector3(x - s, y - s, z - s); };
+		Vector3 operator * (float s) const { return Vector3(x * s, y * s, z * s); };
+		Vector3 operator / (float s) const { return Vector3(x / s, y / s, z / s); };
 
-		//for floats
-		Vector2 operator + (float s) const { return Vector2(x + s, y + s); }
-		Vector2 operator - (float s) const { return Vector2(x - s, y - s); }
-		Vector2 operator / (float s) const { return Vector2(x / s, y / s); }
-		Vector2 operator * (float s) const { return Vector2(x * s, y * s); }
-
-		// ?= operators
-		Vector2& operator += (const Vector2& v) { x += v.x; y += v.y; return *this; }
-		Vector2& operator -= (const Vector2& v) { x -= v.x; y -= v.y; return *this; }
-		Vector2& operator /= (const Vector2& v) { x /= v.x; y /= v.y; return *this; }
-		Vector2& operator *= (const Vector2& v) { x *= v.x; y *= v.y; return *this; }
+		Vector3& operator += (const Vector3& v) { x += v.x, y += v.y, z += v.z; return *this; };
+		Vector3& operator -= (const Vector3& v) { x -= v.x, y -= v.y, z -= v.z; return *this; };
+		Vector3& operator *= (const Vector3& v) { x *= v.x, y *= v.y, z *= v.z; return *this; };
+		Vector3& operator /= (const Vector3& v) { x /= v.x, y /= v.y, z /= v.z; return *this; };
 
 		float LengthSqr() const { return (x * x) + (y * y); };
-		float length() const { return std::sqrt(LengthSqr()); }
+		float Length() const { return sqrt(LengthSqr()); };
 
-		float DistanceSqr(const Vector2& v) { return (v - *this).LengthSqr(); }
-		float Distance(const Vector2& v) { return sqrt(DistanceSqr(v)); }
+		float DistanceSqr(const Vector3& v) const { return (v - *this).LengthSqr(); };
+		float Distance(const Vector3& v) const { return (v - *this).Length(); };
 
-		Vector2 Normalized()const { return *this / length(); } /// becomes 1.0 when normalized
-		void Normalize() { *this /= length(); }
 
-		float Angle() const { return std::atan2f(y, x); }
-		Vector2 Rotate(float radians) const;
+		Vector3 Normalized() const { return *this / Length(); } //returns normalized value of our vector
+		void Normalize() { *this /= Length(); } //has length of 1
 
-		static float SignedAngle(const Vector2& v1, const Vector2& v2);
-		static float Angle(const Vector2& v1, const Vector2& v2);
-		static float Dot(const Vector2& v1, const Vector2& v2);
+		static float Dot(const Vector3& v1, const Vector3& v2);
 
-	private:
 	};
-	inline Vector2 Vector2::Rotate(float radians) const
+
+	// get the dot product between v1 and v2 https://www.falstad.com/dotproduct/
+	inline float Vector3::Dot(const Vector3& v1, const Vector3& v2)
 	{
-		float _x = x * std::cos(radians) - y * std::sin(radians);
-		float _y = x * std::sin(radians) + y * std::cos(radians); //MATH!!
-		return { _x,_y };
+		return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 	}
 
-	// get the unsigned angle in radians between the normalized v1 and normalized v2
-	inline float Vector2::Angle(const Vector2& v1, const Vector2& v2)
-	{
-		return std::acos(Dot(v1, v2));
-	}
-
-	// get the signed counterclockwise angle in radians between v1 and v2
-	inline float Vector2::SignedAngle(const Vector2& v1, const Vector2& v2)
-	{
-		float y = v1.x * v2.y - v1.y * v2.x;
-		float x = v1.x * v2.x + v1.y * v2.y;
-
-		return std::atan2(y, x);
-	}
-
-	// get the dot product beteen v1 and v2 https://www.falstad.com/dotproduct/
-	inline float Vector2::Dot(const Vector2& v1, const Vector2& v2)
-	{
-		return v1.x * v2.x + v1.y * v2.y;
-	}
-
-	inline 	std::istream& operator >> (std::istream& stream, Vector2& v)
+	inline std::istream& operator >> (std::istream& stream, Vector3& v)
 	{
 		std::string line;
 		std::getline(stream, line);
+
 		// { ##, ## }
-	//gets a substring for the numbers in-between the brackets for the numbers
 		std::string xs = line.substr(line.find("{") + 1, line.find(",") - (line.find("{") + 1));
 		v.x = std::stof(xs);
 
 		std::string ys = line.substr(line.find(",") + 1, line.find("}") - (line.find(",") + 1));
 		v.y = std::stof(ys);
+
+		std::string zs = line.substr(line.find(",") + 1, line.find("}") - (line.find(",") + 1));
+		v.z = std::stof(zs);
+
 		return stream;
 	}
 
-	using vec2 = Vector2;
+	using vec3 = Vector3;
 }
